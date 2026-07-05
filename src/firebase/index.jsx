ae1a737538e22657f09eeb685aa0617c4cdb23ff
enes-arisoy <enes.arisoy.85@gmail.com>
2026-07-05 11:46:17 +0300

    updated

--- src/firebase/index.jsx
+++ src/firebase/index.jsx
@@ -6,12 +6,12 @@ import { getStorage} from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbqffbGiaDuytCu1a01YCjSS7KQzPIu4Q",
-  authDomain: "e-commerce-firebase-824f0.firebaseapp.com",
-  projectId: "e-commerce-firebase-824f0",
-  storageBucket: "e-commerce-firebase-824f0.appspot.com",
-  messagingSenderId: "937355028591",
-  appId: "1:937355028591:web:92ac6668279957dd2b859a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
